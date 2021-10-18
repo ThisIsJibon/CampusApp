078162657eaaec027f8657d5a6dd27d11498b1db
Mehedi Hasan <jibonmh7@gmail.com>
2021-10-18 11:15:41 +0600

    blog details layout added

---
+++ app/src/main/java/com/example/campusapp/BlogDetailsActivity.kt
@@ -0,0 +1,28 @@
+package com.example.campusapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_blog_details.*
+
+class BlogDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blog_details)
+        loadUI()
+
+    }
+    fun loadUI(){
+        textAuthor.setText("Mehedi Hasan")
+        blogTitleTextView.setText("Hello from Sydney!")
+        imageAvatar.setImageResource(R.drawable.avatar)
+        dateTextView.setText("20 October, 2021")
+        blogImageView.setImageResource(R.drawable.sydney_image)
+        textRating.setText("4.4")
+        textViews.setText("(420 views)")
+        textDescription.setText("Australia is one of the most popular travel destinations in the world.")
+        blogRatingBar.setRating(4.4f)
+        backIcon.setOnClickListener { v->finish() }
+
+    }
+}
